ff81ab33f5d135ed87410566efe7ea1ba9d88788
Wose <Jacksonteather@hotmail.com>
2017-11-08 23:58:24 -0500

    🚿 Move JakeHead SVG bits into JSON

--- components/jake-head.vue
+++ components/jake-head.vue
@@ -1,145 +1,15 @@
 <template>
   <svg class="jake" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1920 1080" preserveAspectRatio="xMidYMid slice">
     <defs>
-      <clipPath id="06b07a17-56fa-49fe-ba82-026b74cbd24a">
-        <polygon points="1193.203 1077.187 1163.672 1020.938 1023.047 957.656 954.844 898.594 895.781 892.969 966.094 951.328 939.375 990 813.516 1025.859 625.781 973.125 487.969 911.953 570.938 830.391 592.031 742.5 597.647 649.688 530.859 518.203 530.859 469.688 542.109 438.75 510.469 393.047 506.25 265.781 566.719 144.844 615.938 120.234 611.719 49.922 583.936 0 0 0 0 1080 1195.293 1080 1193.203 1077.187" style="fill: none"/>
-      </clipPath>
-      <clipPath id="19b4dfb4-e56e-4e24-8734-5e21c922ded3">
-        <polygon points="797.344 225.703 615.938 120.234 638.438 92.109 787.5 60.469 945.703 165.938 1004.837 248.701 797.344 225.703" style="fill: none"/>
-      </clipPath>
-      <clipPath id="e5d4d403-c2ef-4ba7-a2eb-cd85670ce49b">
-        <polygon points="743.906 436.641 780.469 393.047 883.828 372.656 923.906 425.391 883.828 473.906 743.906 436.641" style="fill: none"/>
-      </clipPath>
-      <clipPath id="5d0e2580-80f9-4398-b2b6-45b8c6cdfe74">
-        <polygon points="962.578 432.422 1001.25 383.906 1046.25 405 1026.029 464.623 971.719 464.623 962.578 432.422" style="fill: none"/>
-      </clipPath>
-      <clipPath id="b88933ad-ac3f-4850-b219-86c42535cc7a">
-        <polygon points="729.141 308.672 780.469 393.047 883.828 372.656 954.141 324.844 962.578 244.017 797.344 225.703 729.141 308.672" style="fill: none"/>
-      </clipPath>
-      <clipPath id="b2685731-9ab3-4cd1-b239-e0f5de3901a1">
-        <polygon points="1046.25 405 1004.837 248.701 962.578 244.017 954.141 324.844 1001.25 383.906 1046.25 405" style="fill: none"/>
-      </clipPath>
-      <clipPath id="302e4ccd-7be4-4f1b-b0b2-2adc4b89a4d5">
-        <polygon points="923.906 425.391 962.578 432.422 1001.25 383.906 954.141 324.844 883.828 372.656 923.906 425.391" style="fill: none"/>
-      </clipPath>
-      <clipPath id="3eb2d99f-d270-4728-855d-88e649dcc2a1">
-        <polygon points="1009.687 537.187 971.719 464.623 962.578 432.422 923.906 425.391 883.828 473.906 852.187 544.219 878.203 601.172 978.75 587.109 1009.687 537.187" style="fill: none"/>
-      </clipPath>
-      <clipPath id="55b34933-587e-439f-80a6-9efdfd1c3b00">
-        <polygon points="1010.391 618.047 978.75 587.109 878.203 601.172 839.531 648.984 878.906 823.359 893.187 841.594 960.382 802.59 1016.719 653.203 1010.391 618.047" style="fill: none"/>
-      </clipPath>
-      <clipPath id="db693a84-2fd0-44cd-a310-b5e1ad765ad5">
-        <polygon points="1009.687 537.187 1042.734 522.422 1010.391 618.047 978.75 587.109 1009.687 537.187" style="fill: none"/>
-      </clipPath>
-      <clipPath id="a71560c1-a882-44e8-b715-73fa58df038a">
-        <polygon points="971.719 464.623 1026.029 464.623 1042.734 522.422 1009.687 537.187 971.719 464.623" style="fill: none"/>
-      </clipPath>
-      <clipPath id="d4e3029e-5d1b-48f3-a56e-3d0c4c1e66a0">
-        <polygon points="729.141 308.672 510.469 393.047 506.25 265.781 566.719 144.844 615.938 120.234 797.344 225.703 729.141 308.672" style="fill: none"/>
-      </clipPath>
-      <clipPath id="f1dededb-86c0-49bd-b465-7fceba40939f">
-        <polygon points="530.859 469.687 542.109 438.75 630 457.734 672.891 566.016 644.766 658.125 597.647 649.687 530.859 518.203 530.859 469.687" style="fill: none"/>
-      </clipPath>
-      <clipPath id="8299c243-9c77-4e66-9f5f-f092aa17bc70">
-        <polygon points="510.469 393.047 542.109 438.75 630 457.734 743.906 436.641 780.469 393.047 729.141 308.672 510.469 393.047" style="fill: none"/>
-      </clipPath>
-      <clipPath id="37cd1d01-3ea3-41ef-959d-589fe8536aa4">
-        <polygon points="672.891 566.016 852.187 544.219 883.828 473.906 743.906 436.641 630 457.734 672.891 566.016" style="fill: none"/>
-      </clipPath>
-      <clipPath id="06ebefb8-ae6f-44ce-ae2a-2085da8aed37">
-        <polygon points="839.531 648.984 678.516 776.953 644.766 658.125 672.891 566.016 852.187 544.219 878.203 601.172 839.531 648.984" style="fill: none"/>
-      </clipPath>
-      <clipPath id="677a72b6-8ff0-414c-a24a-3e85111e3d21">
-        <polygon points="878.906 823.359 843.047 836.016 678.516 776.953 839.531 648.984 878.906 823.359" style="fill: none"/>
-      </clipPath>
-      <clipPath id="ad77ee41-cf62-43e6-8ec2-492a31ec7ff7">
-        <polygon points="895.781 892.969 966.094 951.328 939.375 990 813.516 1025.859 784.687 892.969 895.781 892.969" style="fill: none"/>
-      </clipPath>
-      <clipPath id="e68bc29c-e265-44e5-af30-baa87e4fff6b">
-        <polygon points="843.047 836.016 843.047 892.969 895.781 892.969 893.187 841.594 878.906 823.359 843.047 836.016" style="fill: none"/>
-      </clipPath>
-      <clipPath id="5f5d3167-7918-4757-815e-c588d8f97fad">
-        <polygon points="784.687 892.969 805.34 822.48 843.047 836.016 843.047 892.969 784.687 892.969" style="fill: none"/>
-      </clipPath>
-      <clipPath id="00f55768-bb4c-4b68-bce6-3212637a2629">
-        <polygon points="625.781 973.125 784.687 892.969 805.34 822.48 678.516 776.953 570.937 830.391 487.969 911.953 625.781 973.125" style="fill: none"/>
-      </clipPath>
-      <clipPath id="7d6a2b91-b6f6-4a9e-acf7-0a6dac3a1164">
-        <polygon points="592.031 742.5 597.647 649.687 644.766 658.125 678.516 776.953 570.937 830.391 592.031 742.5" style="fill: none"/>
-      </clipPath>
-      <clipPath id="4f68a50f-f47a-4fd7-bb76-6ca4b91ae3de">
-        <polygon points="813.516 1025.859 625.781 973.125 784.687 892.969 813.516 1025.859" style="fill: none"/>
+      <clipPath v-for="(node, index) in svg" :key="index" :id="`clipPath-${index}`">
+        <polygon :points="node.points" style="fill: none"/>
       </clipPath>
       <image id="jake-master" width="1920" height="1368" :xlink:href="src"/>
     </defs>
-    <title>jakey_polygons</title>
-    <g style="clip-path: url(#06b07a17-56fa-49fe-ba82-026b74cbd24a)">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#19b4dfb4-e56e-4e24-8734-5e21c922ded3)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#e5d4d403-c2ef-4ba7-a2eb-cd85670ce49b)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#5d0e2580-80f9-4398-b2b6-45b8c6cdfe74)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#b88933ad-ac3f-4850-b219-86c42535cc7a)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#b2685731-9ab3-4cd1-b239-e0f5de3901a1)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#302e4ccd-7be4-4f1b-b0b2-2adc4b89a4d5)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#3eb2d99f-d270-4728-855d-88e649dcc2a1)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#55b34933-587e-439f-80a6-9efdfd1c3b00)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#db693a84-2fd0-44cd-a310-b5e1ad765ad5)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#a71560c1-a882-44e8-b715-73fa58df038a)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#d4e3029e-5d1b-48f3-a56e-3d0c4c1e66a0)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#f1dededb-86c0-49bd-b465-7fceba40939f)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#8299c243-9c77-4e66-9f5f-f092aa17bc70)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#37cd1d01-3ea3-41ef-959d-589fe8536aa4)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#06ebefb8-ae6f-44ce-ae2a-2085da8aed37)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#677a72b6-8ff0-414c-a24a-3e85111e3d21)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#ad77ee41-cf62-43e6-8ec2-492a31ec7ff7)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#e68bc29c-e265-44e5-af30-baa87e4fff6b)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#5f5d3167-7918-4757-815e-c588d8f97fad)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#00f55768-bb4c-4b68-bce6-3212637a2629)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#7d6a2b91-b6f6-4a9e-acf7-0a6dac3a1164)" v-shift="shiftValue">
-      <use transform="scale(0.78947)" xlink:href="#jake-master"/>
-    </g>
-    <g class="jake-slice" style="clip-path: url(#4f68a50f-f47a-4fd7-bb76-6ca4b91ae3de)" v-shift="shiftValue">
+    <g v-for="(node, index) in svg" :key="index" :style="{
+      clipPath: `url(#clipPath-${index})`,
+      transformOrigin: node.style.transformOrigin
+    }">
       <use transform="scale(0.78947)" xlink:href="#jake-master"/>
     </g>
   </svg>
@@ -168,7 +38,8 @@ export default {
       shiftValue: {
         x: 0,
         y: 0
-      }
+      },
+      svg
     }
   },
   mounted () {
